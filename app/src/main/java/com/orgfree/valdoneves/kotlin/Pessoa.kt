8f36821754ff2bdb712ba31548079be7eff1d53d
Valdo Neves <valdogt1@gmail.com>
2021-04-27 22:00:14 -0300

    comentando o codigo

--- app/src/main/java/com/orgfree/valdoneves/kotlin/Pessoa.kt
+++ app/src/main/java/com/orgfree/valdoneves/kotlin/Pessoa.kt
@@ -1,13 +1,18 @@
 package com.orgfree.valdoneves.kotlin
 
+//craiando classe pessoa
 class Pessoa {
+
+    //definindo parametros com seus tipos (testes)
     var nome: String = "Eu"
     var cpf:String = "123.123.123-12"
 }
 
 fun main(){
+    //instanciando a classe pessoa
     val eu = Pessoa()
 
+    //recebendo os valores da classe
     println(eu.nome)
     println(eu.cpf)
 }
