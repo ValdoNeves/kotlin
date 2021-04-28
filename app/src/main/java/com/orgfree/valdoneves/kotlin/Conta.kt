8fe97254fd50a2ddaac730a9bb762300f68458bd
Valdo Neves <valdogt1@gmail.com>
2021-04-27 23:57:34 -0300

    criando uma classe conta

---
+++ app/src/main/java/com/orgfree/valdoneves/kotlin/Conta.kt
@@ -0,0 +1,15 @@
+package com.orgfree.valdoneves.kotlin
+
+import java.math.BigDecimal
+
+class Conta (val agencia: String,val numero:String, val saldo: BigDecimal){
+
+    fun deposito(valor: BigDecimal){
+
+    }
+
+    fun saque(valor: BigDecimal){
+        
+    }
+
+}
